da316aa45974c5a54a38ccad69fb7e2a0fd14bde
Mounir Kalai <57255735+Mounir070@users.noreply.github.com>
2021-09-15 09:57:49 +0000

    class gemaakt

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 var x, y, vx, vy;
+var ball1, ball2, ball3;
+
+class Ball{
+  constructor(x, y, w, h, vx, vy) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = vx;
+    this.vy = vy;
+  }
+function setup(){
+ createCanvas(400, 400);
+ 
+  ball1 = new Ball(30,200,50,50,5,5,"red")
+
+  ball2 = new Ball(30,200,50,50,5,5,"bleu")
+
+  ball3 = new Ball(30,200,50,50,5,5,"red")
+
+}
+
+funtion draw(){
+  background(225);
+
+  ball1.draw();
+  ball2.draw();
+  ball3.draw();
+
+}
 
 function setup() {
 	createCanvas(400, 400);
@@ -7,7 +37,9 @@ function setup() {
   y = 200;
   vx = 5;
   vy = 5;
+
 }
+
 
 function draw() {
 	background(225);
